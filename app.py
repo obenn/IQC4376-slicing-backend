57292a176792b5af61cf7712384b05e167a77432
Oliver Benning <obenn009@uottawa.ca>
2020-10-16 02:07:22 -0400

    Filter

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
-import basa_data_api as api
+import pandas as pd
+import os
 app = Flask(__name__)
 CORS(app)
-api.df = api.init_df()
+print("Loading df into memory")
+csv_location=os.getenv('SLICING_DATA_LOCATION', 'basa_data.csv')
+df = pd.read_csv(csv_location, low_memory=False)
+print("done")
 
 @app.route('/')
 def hello_world():
@@ -11,31 +15,28 @@ def hello_world():
 
 @app.route('/columns')
 def columns():
-    return jsonify(api.columns())
+    return jsonify(list(df))
+
+def get_subset(args):
+    beginning = int(args.get('s', 0))
+    end = int(args.get('e', len(df.index)))
+    columns = args.get('c', None)
+    filter_ = args.get('f', None)
+    if not columns:
+        columns = list(df)
+    else:
+        columns = columns.split(',')
+    dfs = (df.query(filter_) if filter_ else df).iloc[beginning:end][columns]
+    return dfs, len(dfs.index) 
 
 @app.route('/preview')
 def preview():
-    beginning = int(request.args.get('beginning', 0))
-    end = int(request.args.get('end', 1000))
-    columns = request.args.get('columns', None)
-    if not columns:
-        columns = api.columns()
-    else:
-        columns = columns.split(',')
-    dfs = api.subset(beginning=beginning, end=end, columns=columns)
-    return dfs[0:10].to_html()
-    #return jsonify(dfs[0:10].to_dict(orient="records"))
+    dfs, length = get_subset(request.args)
+    return f'<p>Number of rows: {length}</p>' + dfs.head(n=100).to_html()
 
 @app.route('/subset')
 def subset():
-    beginning = int(request.args.get('beginning', 0))
-    end = int(request.args.get('end', len(api.df.index)))
-    columns = request.args.get('columns', None)
-    if not columns:
-        columns = api.columns()
-    else:
-        columns = columns.split(',')
-    dfs = api.subset(beginning=beginning, end=end, columns=columns)
+    dfs, length = get_subset(request.args)
     res = make_response(dfs.to_csv())
     res.headers["Content-Disposition"] = "attachment; filename=export.csv"
     res.headers["Content-Type"] = "text/csv"
